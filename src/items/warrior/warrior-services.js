d1455043d5e6b0e2f120888f598eb7aa6cdddb4e
Kevin Enloe <enloe92@gmail.com>
2021-02-15 16:16:55 -0700

    new

--- src/items/warrior/warrior-services.js
+++ src/items/warrior/warrior-services.js
@@ -1,15 +1,92 @@
 const warriorServices = {
-  getFuryWarriorArmor(db){
+  getFuryWarriorArmor(db) {
     const gear = [];
-    const helm = 
+    const helm =
       db
-        .from('wow_gear')
-        .select('*')
-        .where('item_name', 'ILIKE', 'Diadem of Imperious Desire')
-    
-    return helm; 
+      .from('wow_gear')
+      .select('*')
+      .where('item_name', 'ILIKE', 'Diadem of Imperious Desire')
+    const neck =
+      db
+      .from('wow_gear')
+      .select('*')
+      .where('item_name', 'ILIKE', 'Charm of Eternal Winter')
+
+    const shoulder =
+      db
+      .from('wow_gear')
+      .select('*')
+      .where('item_name', 'ILIKE', 'Epaulettes of Overwhelming Force')
+
+    const cloak =
+      db
+      .from('wow_gear')
+      .select('*')
+      .where('item_name', 'ILIKE', 'Crest of the Legionnaire General')
+
+    const chest =
+      db
+      .from('wow_gear')
+      .select('*')
+      .where('item_name', 'ILIKE', 'Breastplate of Cautious Calculation')
+
+    const wrists =
+      db
+      .from('wow_gear')
+      .select('*')
+      .where('item_name', 'ILIKE', 'Hellhound Cuffs')
+
+    const gloves =
+      db
+      .from('wow_gear')
+      .select('*')
+      .where('item_name', 'ILIKE', 'Colossal Plate Gauntlets')
+
+    const belt =
+      db
+      .from('wow_gear')
+      .select('*')
+      .where('item_name', 'ILIKE', 'Binding of Warped Desires')
+
+    const legs =
+      db
+      .from('wow_gear')
+      .select('*')
+      .where('item_name', 'ILIKE', 'Endlessly Gluttonous Greaves')
+
+    const boots =
+      db
+      .from('wow_gear')
+      .select('*')
+      .where('item_name', 'ILIKE', 'Errant Crusaders Greaves')
+
+    const ring1 =
+      db
+      .from('wow_gear')
+      .select('*')
+      .where('item_name', 'ILIKE', 'Most Regal Signet of Sire Denathrius')
+
+    const ring2 =
+      db
+      .from('wow_gear')
+      .select('*')
+      .where('item_name', 'ILIKE', 'Ritualists Treasured Ring')
+
+    const weapon1 =
+      db
+      .from('wow_gear')
+      .select('*')
+      .where('item_name', 'ILIKE', 'Nathrian Crusaders Bastard Sword')
+
+    const weapon2 =
+      db
+      .from('wow_gear')
+      .select('*')
+      .where('item_name', 'ILIKE', 'Nathrian Crusaders Bastard Sword')
+
+    gear.push(helm, neck, shoulder, cloak, chest, wrist, gloves, belt, legs, boots, ring1, ring2, weapon1, weapon2)
+    return gear;
   }
 }
 
 module.exports = warriorServices
-
