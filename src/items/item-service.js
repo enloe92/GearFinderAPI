0c20d552d2708175472c6a71b43ff4a6e0ab9f3a
Kevin Enloe <enloe92@gmail.com>
2021-02-15 15:51:04 -0700

    new

--- src/items/item-service.js
+++ src/items/item-service.js
@@ -41,8 +41,7 @@ const ItemsService = {
     return db
       .from('wow_gear')
       .select('*')
-      .where('item_type', 'ILIKE', 'Plate')
-      .and('item_type', 'ILIKE', 'All')
+      .where('item_type', 'ILIKE', 'Plate, All')
       .where('primary_stat', 'ILIKE', 'Haste')  
   }
 }
