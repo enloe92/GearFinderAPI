03f4c1cf84b1df2b1aa042d8086e736c934b9d2e
Kevin Enloe <enloe92@gmail.com>
2021-02-15 17:22:18 -0700

    new

--- migrations/004.do.create_bis_lists.sql
+++ migrations/004.do.create_bis_lists.sql
@@ -1,116 +1,467 @@
 CREATE TABLE fury_warrior(
-    item_name TEXT REFERENCES wow_gear(item_name),
-    item_category TEXT REFERENCES wow_gear(item_category),
-    item_type TEXT REFERENCES wow_gear(item_type),
-    item_class TEXT REFERENCES wow_gear(item_class),
-    item_location TEXT REFERENCES wow_gear(item_location),
-    item_source TEXT REFERENCES wow_gear(item_source),
-    main_stat TEXT REFERENCES wow_gear(main_stat),
-    primary_stat TEXT REFERENCES wow_gear(primary_stat),
-    secondary_stat TEXT REFERENCES wow_gear(secondary_stat),
-    item_id TEXT REFERENCES wow_gear(item_id)
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
 );
 
 CREATE TABLE arms_warrior(
-    item_name TEXT REFERENCES wow_gear(item_name),
-    item_category TEXT REFERENCES wow_gear(item_category),
-    item_type TEXT REFERENCES wow_gear(item_type),
-    item_class TEXT REFERENCES wow_gear(item_class),
-    item_location TEXT REFERENCES wow_gear(item_location),
-    item_source TEXT REFERENCES wow_gear(item_source),
-    main_stat TEXT REFERENCES wow_gear(main_stat),
-    primary_stat TEXT REFERENCES wow_gear(primary_stat),
-    secondary_stat TEXT REFERENCES wow_gear(secondary_stat),
-    item_id TEXT REFERENCES wow_gear(item_id)
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
 );
 
 CREATE TABLE protection_warrior(
-    item_name TEXT REFERENCES wow_gear(item_name),
-    item_category TEXT REFERENCES wow_gear(item_category),
-    item_type TEXT REFERENCES wow_gear(item_type),
-    item_class TEXT REFERENCES wow_gear(item_class),
-    item_location TEXT REFERENCES wow_gear(item_location),
-    item_source TEXT REFERENCES wow_gear(item_source),
-    main_stat TEXT REFERENCES wow_gear(main_stat),
-    primary_stat TEXT REFERENCES wow_gear(primary_stat),
-    secondary_stat TEXT REFERENCES wow_gear(secondary_stat),
-    item_id TEXT REFERENCES wow_gear(item_id)
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
 );
 
 CREATE TABLE marksman_hunter(
-    item_name TEXT REFERENCES wow_gear(item_name),
-    item_category TEXT REFERENCES wow_gear(item_category),
-    item_type TEXT REFERENCES wow_gear(item_type),
-    item_class TEXT REFERENCES wow_gear(item_class),
-    item_location TEXT REFERENCES wow_gear(item_location),
-    item_source TEXT REFERENCES wow_gear(item_source),
-    main_stat TEXT REFERENCES wow_gear(main_stat),
-    primary_stat TEXT REFERENCES wow_gear(primary_stat),
-    secondary_stat TEXT REFERENCES wow_gear(secondary_stat),
-    item_id TEXT REFERENCES wow_gear(item_id)
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
 );
 
 CREATE TABLE beast_mastery_hunter(
-    item_name TEXT REFERENCES wow_gear(item_name),
-    item_category TEXT REFERENCES wow_gear(item_category),
-    item_type TEXT REFERENCES wow_gear(item_type),
-    item_class TEXT REFERENCES wow_gear(item_class),
-    item_location TEXT REFERENCES wow_gear(item_location),
-    item_source TEXT REFERENCES wow_gear(item_source),
-    main_stat TEXT REFERENCES wow_gear(main_stat),
-    primary_stat TEXT REFERENCES wow_gear(primary_stat),
-    secondary_stat TEXT REFERENCES wow_gear(secondary_stat),
-    item_id TEXT REFERENCES wow_gear(item_id)
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
 );
 
 CREATE TABLE survival_hunter(
-    item_name TEXT REFERENCES wow_gear(item_name),
-    item_category TEXT REFERENCES wow_gear(item_category),
-    item_type TEXT REFERENCES wow_gear(item_type),
-    item_class TEXT REFERENCES wow_gear(item_class),
-    item_location TEXT REFERENCES wow_gear(item_location),
-    item_source TEXT REFERENCES wow_gear(item_source),
-    main_stat TEXT REFERENCES wow_gear(main_stat),
-    primary_stat TEXT REFERENCES wow_gear(primary_stat),
-    secondary_stat TEXT REFERENCES wow_gear(secondary_stat),
-    item_id TEXT REFERENCES wow_gear(item_id)
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
 );
 
 CREATE TABLE holy_priest(
-    item_name TEXT REFERENCES wow_gear(item_name),
-    item_category TEXT REFERENCES wow_gear(item_category),
-    item_type TEXT REFERENCES wow_gear(item_type),
-    item_class TEXT REFERENCES wow_gear(item_class),
-    item_location TEXT REFERENCES wow_gear(item_location),
-    item_source TEXT REFERENCES wow_gear(item_source),
-    main_stat TEXT REFERENCES wow_gear(main_stat),
-    primary_stat TEXT REFERENCES wow_gear(primary_stat),
-    secondary_stat TEXT REFERENCES wow_gear(secondary_stat),
-    item_id TEXT REFERENCES wow_gear(item_id)
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
 );
 
 CREATE TABLE discipline_priest(
-    item_name TEXT REFERENCES wow_gear(item_name),
-    item_category TEXT REFERENCES wow_gear(item_category),
-    item_type TEXT REFERENCES wow_gear(item_type),
-    item_class TEXT REFERENCES wow_gear(item_class),
-    item_location TEXT REFERENCES wow_gear(item_location),
-    item_source TEXT REFERENCES wow_gear(item_source),
-    main_stat TEXT REFERENCES wow_gear(main_stat),
-    primary_stat TEXT REFERENCES wow_gear(primary_stat),
-    secondary_stat TEXT REFERENCES wow_gear(secondary_stat),
-    item_id TEXT REFERENCES wow_gear(item_id)
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
 );
 
 CREATE TABLE shadow_priest(
-    item_name TEXT REFERENCES wow_gear(item_name),
-    item_category TEXT REFERENCES wow_gear(item_category),
-    item_type TEXT REFERENCES wow_gear(item_type),
-    item_class TEXT REFERENCES wow_gear(item_class),
-    item_location TEXT REFERENCES wow_gear(item_location),
-    item_source TEXT REFERENCES wow_gear(item_source),
-    main_stat TEXT REFERENCES wow_gear(main_stat),
-    primary_stat TEXT REFERENCES wow_gear(primary_stat),
-    secondary_stat TEXT REFERENCES wow_gear(secondary_stat),
-    item_id TEXT REFERENCES wow_gear(item_id)
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE elemental_shaman(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE enhancement_shaman(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE restoration_shaman(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE demonology_warlock(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE destruction_warlock(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE affliction_warlock(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE feral_druid(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE guardian_druid(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE balance_druid(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE restoration_druid(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE assasination_rogue(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE sublety_rogue(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE outlaw_rogue(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE protection_paladin(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE retribution_paladin(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE holy_paladin(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE vengeance_demon_hunter(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE havoc_demon_hunter(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE blood_death_knight(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE frost_death_knight(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE unholy_death_knight(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE fire_mage(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE frost_mage(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE arcane_mage(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE windwalker_monk(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE brewmaster_monk(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
+);
+
+CREATE TABLE mistweaver_monk(
+    item_name TEXT NOT NULL,
+    item_category TEXT NOT NULL,
+    item_type TEXT NOT NULL,
+    item_class TEXT NOT NULL,
+    item_location TEXT NOT NULL,
+    item_source TEXT NOT NULL,
+    main_stat TEXT NOT NULL,
+    primary_stat TEXT NOT NULL,
+    secondary_stat TEXT NOT NULL,
+    item_id TEXT NOT NULL
 );
